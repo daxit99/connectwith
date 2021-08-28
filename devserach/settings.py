99fa215d5435ca52f79010dc8a70e38e5d71ed48
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-08-28 15:12:26 +0500

    change settings

--- devserach/settings.py
+++ devserach/settings.py
@@ -119,23 +119,23 @@ WSGI_APPLICATION = 'devserach.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'connect',
-        'USER':'postgres',
-        'PASSWORD':'daxit123',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'connect',
+#         'USER':'postgres',
+#         'PASSWORD':'daxit123',
+#         'HOST':'localhost',
+#         'PORT':'5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
